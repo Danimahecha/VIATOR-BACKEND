50695ce64c5ea650aed03512ea430379d8c78125
JulioXC4 <danielc4nt@gmail.com>
2023-02-21 17:15:53 -0500

    archivos eliminados de la carpeta routes

--- src/routes/fligths.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const Route = Router()
-const{getFlight, getFlights, updateFlight, createFlight, deleteFligth}= require('../controllers/flights.controllers')
-Route.get('/', getFlights);
-Route.get('/:id', getFlight);
-Route.post('/', createFlight);
-Route.put('/:id', updateFlight);
-Route.delete('/:id', deleteFligth);
