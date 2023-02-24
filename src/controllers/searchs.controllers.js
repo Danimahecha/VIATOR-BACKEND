d0bdb5491f371b2dca53aa9ed824cfd668bbf2b2
daniel mahecha <daniel.mahecham@gmail.com>
2023-02-24 12:46:27 -0500

    buscar aereopuertos por ciudad segun la aereolinea

--- src/controllers/searchs.controllers.js
+++ src/controllers/searchs.controllers.js
@@ -1,6 +1,5 @@
-const {Airport, Airline, Flight} = require('../db.js');
+const {Airport, Flight} = require('../db.js');
 const { Op } = require('sequelize');
-const moment = require('moment');
 
     const getAirportsByInput = async (req, res) => {
 
@@ -22,46 +21,30 @@ const moment = require('moment');
               if(airports.length === 0) return res.status(404).send({massage:'There are no airports with this name'})
               else res.status(200).send(airports);
 
+            /* const airportsPerCountries = await Airport.findAll({
+                where: {country: { [Op.like]: `%${input}%` }},
+            })
+
+            if(!airportsPerCountries){
+                const aiportsPerCity = await Airport.findAll({
+                    where: {city: { [Op.like]: `%${input}%` }},
+                })
+
+                if(!aiportsPerCity) return res.status(404).send({massage:'There are no airports'})
+                else res.status(200).send(aiportsPerCity);
+            }
+
+            if(airportsPerCountries) res.status(200).send(airportsPerCountries); */
+
         }catch(error){
 
             return res.status(400).send({message: error.message});
 
         }
     };
+  
 
-    const getFlightsByQuery = async (req, res) => {
-
-        const {origin, destiny, dateTimeDeparture, dateTimeArrival, passengers} = req.query
-
-        const dateFormat = 'YYYY-MM-DD HH:mm:ss'
-
-        const ConvertedDateTimeDeparture = moment(dateTimeDeparture).format(dateFormat)
-        const ConvertedDateTimeArrival = moment(dateTimeArrival).format(dateFormat)
-
-        try {
-            if(origin && destiny && passengers){
-                const flights = await Flight.findAll({
-                    where: {
-                      [Op.and]: [
-                        { origin: { [Op.like]: `%${origin}%` } },
-                        { destiny: { [Op.like]: `%${destiny}%` } },
-                        { dateTimeDeparture: { [Op.eq]: ConvertedDateTimeDeparture} },
-                        { dateTimeArrival: { [Op.eq]: ConvertedDateTimeArrival} },
-                        { seatsAvailable: { [Op.eq]: passengers } },
-                      ]
-                    }
-                  })
-
-                  res.status(200).send(flights)
-            }
-        } catch (error) {
-
-          return res.status(400).send({message: error.message});
-
-        }
-    }
 
 module.exports = {
     getAirportsByInput,
-    getFlightsByQuery
   };
