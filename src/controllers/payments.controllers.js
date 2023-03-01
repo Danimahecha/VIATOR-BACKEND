3afb49fb1ecfebef0383dcada95cbb656ff1e732
JulioXC4 <danielc4nt@gmail.com>
2023-03-01 12:54:59 -0500

    payment

--- src/controllers/payments.controllers.js
+++ src/controllers/payments.controllers.js
@@ -23,13 +23,11 @@ const {PAYPAL_CLIENT_ID, PAYPAL_SECRET, PAYPAL_API, HOST} = process.env
         },
       };
   
-
+        //Obtener token
         const params = new URLSearchParams();
         params.append("grant_type", "client_credentials");
     
-        const {
-          data: { access_token },
-        } = await axios.post(
+        const { data: { access_token },} = await axios.post(
           "https://api-m.sandbox.paypal.com/v1/oauth2/token",
           params,
           {
@@ -43,6 +41,7 @@ const {PAYPAL_CLIENT_ID, PAYPAL_SECRET, PAYPAL_API, HOST} = process.env
           }
         );
     
+        //Enviar la orden
         const response = await axios.post(
           `${PAYPAL_API}/v2/checkout/orders`,
           order,
@@ -53,17 +52,42 @@ const {PAYPAL_CLIENT_ID, PAYPAL_SECRET, PAYPAL_API, HOST} = process.env
           }
         );
     
-        console.log(response.data);
+        console.log(response.data.links[1]);
     
-        return res.json(response.data);
+        return res.json(response.data.links[1]);
 
     }
 
     const captureOrder = async (req, res ) =>{
     
-        res.send('capture order')
+      const { token } = req.query;
+
+      try {
+
+        const response = await axios.post(
+          `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+          {},
+          {
+            auth: {
+              username: PAYPAL_CLIENT_ID,
+              password: PAYPAL_SECRET,
+            },
+          }
+        );
+        
+        console.log(response.data);
+        
+        res.send("/pagina red");
+      } catch (error) {
+
+        console.log(error.message);
+
+        return res.status(500).json({ message: "Internal Server error" });
+      }
+
+    };
      
-    }
+    
 
     const cancelOrder = async (req, res ) =>{
     
