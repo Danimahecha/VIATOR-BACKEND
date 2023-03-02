83c6b1227d2b94f90fa6365c844e07063488701d
daniel mahecha <daniel.mahecham@gmail.com>
2023-03-02 14:13:06 -0500

    fix bugs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios')
 const { addAirportToAirline } = require('../controllers/airlines.controllers')
 const { createAirport, addAirlineToAirport } = require('../controllers/airports.controllers')
 const{Airport, Airline, Flight}= require('../db')
@@ -16,36 +17,6 @@ function FligthRandom(vuelos){
     
 }
 
-async function relationAirports (airportsId, airlineId) {
-  const airline = await Airline.findByPk(airlineId)
-
-  if(airportsId.length >= 1){
-      airportsId.map( async (airportId) => {
-         let airport = await Airport.findByPk(airportId)
-         await airline.addAirport(airport)
-        })
-
-  }else{
-      const airport = await Airport.findByPk(airportsId)
-      await airline.addAirport(airport)
-  }
-}
-async function relationAirlines(airportId, airlinesId) {
-  const airport = await Airport.findByPk(airportId)
-
-            if(airlinesId.length >= 1){
-                airlinesId.map( async (airlineId) => {
-                   let airLine = await Airline.findByPk(airlineId)
-                   await airport.addAirline(airLine)
-                  })
-            }else{
-
-                const airline = await Airline.findByPk(airlinesId)
-                await airport.addAirline(airline)
-
-               
-            }
-  }
 
 const CreateModels=async()=>{
  json.aereopuertos.map(async airP=>{
@@ -55,13 +26,16 @@ json.aereolineas.map(async airL=>{
   await Airline.create(airL)
 });
 
-//   json.relaciones.aereopuertos.map( (rel) => {
-//      relationAirports(rel.airportsId, rel.airlineId)
-//   });
-// json.relaciones.aereolineas.map( rel=>{
-//    relationAirlines(rel.airportId, rel.airlinesId)
-// });
 
+json.relaciones.aereopuertos.map(async (rel)=>{
+await axios.post('http://localhost:4000/api/addAirportToAirline', rel)
+});
+
+
+  
+json.vuelos.map(async F=>{
+await axios.post('http://localhost:4000/api/flights',F)
+})
 }
-
+ 
 module.exports= {FligthRandom, CreateModels}
