f074d07d53fd7cd77fa0e7d87f1ca67559561eb7
JulioXC4 <danielc4nt@gmail.com>
2023-03-07 20:48:46 -0500

    comentada la createmodels

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {PORT} = process.env
 // Syncing all the models at once.
 conn.sync({ force: true}).then(() => {
   server.listen(PORT, () => {
-    CreateModels()
+    //CreateModels()
     console.log(`%s listening at port ${PORT}`); // eslint-disable-line no-console
   });
 });
