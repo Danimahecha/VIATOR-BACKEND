dad9dd8bbc3aa027c559fb54a60ebd9f867bd42b
JulioXC4 <danielc4nt@gmail.com>
2023-03-08 12:10:26 -0500

    Comentado el createModels y force en false

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {PORT} = process.env
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
   server.listen(PORT, () => {
-    CreateModels()
+    //CreateModels()
     console.log(`%s listening at port ${PORT}`); // eslint-disable-line no-console
   });
 });
